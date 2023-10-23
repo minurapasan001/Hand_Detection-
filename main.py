a363711ef8e6a7d67fa5bb6f934822225979ad67
Minura Pasan <105039109+minurapasan001@users.noreply.github.com>
2023-10-23 16:14:54 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,44 @@
+import cv2
+import mediapipe as mp
+import time
+
+cap = cv2.VideoCapture(0)
+
+mpHands = mp.solutions.hands
+hands = mpHands.Hands(model_complexity=0)
+hands = mpHands.Hands(
+    min_detection_confidence=0.2,
+    min_tracking_confidence=0.2
+)
+
+
+mpDraw = mp.solutions.drawing_utils
+
+pTime = 0
+cTime = 0
+
+while True:
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                print(id,lm)
+                h,w,c = img.shape
+                cx,cy = int(lm.x*w), int(lm.y*h)
+                print(cx,cy)
+                if id == 0:
+                    cv2.circle(img, (cx,cy), 15,(255,0,255),cv2.FILLED)
+
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    cv2.putText(img,str(int(fps)) ,(10,70),cv2.FONT_HERSHEY_PLAIN,3,(0,0,0),3)
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
